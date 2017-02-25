b1590a0724bf249fc1f4b5060681ccd28cfe3c77
David Dang <daviddang@Davids-MacBook-Pro.local>
2017-02-24 20:31:31 -0800

    Assignment completed.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
@@ -7,8 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/movie', methods = ['POST'])
+@app.route('/movie')
 def movie():
+    return render_template('movie.html')
+
+@app.route('/add-movie', methods = ['POST'])
+def addmovie():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
 
@@ -16,19 +20,35 @@ def movie():
         title = request.form['title']
         releaseyear = request.form['releaseyear']
         description = request.form['description']
-        print('hello')
-        cursor.execute('INSERT INTO movie (title, releaseyear, description) VALUES (?, ?, ?)', (title, releaseyear, description))
+        cursor.execute('INSERT INTO movies (title, releaseyear, description) VALUES (?, ?, ?)', (title, releaseyear, description))
         connection.commit()
-        message = 'Successfully added information into movies table'
+        message = 'Successfully added information into the movies table'
     except:
         connection.rollback()
-        message = 'Failed to add information into movies table'
+        message = 'Failed to add information into the movies table'
     finally:
         connection.close()
         return str(message)
 
-app.route('movies')
+@app.route('/movies')
 def movies():
-    return render_tempplate('movies.html')
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute('SELECT * FROM movies')
+        movies = cursor.fetchall()
+        message = 'Movies found'
+    except:
+        connection.rollback()
+        message = 'Failed to find movies'
+    finally:
+        connection.close()
+        return jsonify(movies)
+
+@app.route('/search')
+def search():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
 
 app.run(debug = True)
